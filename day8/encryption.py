45900b6dad8125dcbc7b81a40ef58230e5d30711
Rajdeep Dey <rajdeep.x70@outlook.com>
2025-07-16 17:54:09 +0530

    fix: correct function definition in encryption.py

--- day8/encryption.py
+++ day8/encryption.py
@@ -4,4 +4,14 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt()
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+
+    for letter in original_text:
+        shifted_location= alphabet.index(letter) + shift_amount
+        shifted_location %= len(alphabet)  # Wrap around if it exceeds the length of the alphabet
+        cipher_text += alphabet[shifted_location]
+    print(f"The encoded text is {cipher_text}")
+encrypt(original_text=text, shift_amount=shift)
+
