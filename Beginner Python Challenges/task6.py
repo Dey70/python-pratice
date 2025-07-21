f9d5af09046623cf06cae0cf9f6cb066a9821837
Rajdeep Dey <rajdeep.x70@outlook.com>
2025-07-21 11:06:11 +0530

    Challenge 6: Dice Roller

---
+++ Beginner Python Challenges/task6.py
@@ -0,0 +1,14 @@
+"""
+🎯 Challenge 6: Dice Roller
+🧠 Problem:
+Simulate rolling a 6-sided die.
+
+Each roll should return a number between 1 and 6.
+
+Bonus: Ask the user if they want to roll again.
+"""
+
+import random
+rolling_dice = random.randint(1,6)
+print("....The dice is rolling..........")
+print(f"The number is :{rolling_dice}")
