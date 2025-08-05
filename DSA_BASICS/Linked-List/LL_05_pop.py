3f3010805a719910b102f5b23d774796c55fe4a9
RAJDEEP DEY <rajdeep.x70@outlook.com>
2025-08-05 14:32:39 +0530

    learn LL: pop()

---
+++ DSA_BASICS/Linked-List/LL_05_pop.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value, end = "->")
+            temp = temp.next
+        print("None")
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp   
+
+
+LL = LinkedList(4)
+LL.append(15)
+LL.append(20)
+LL.print_list()
+
+print(LL.pop().value)  # Should print 20
+LL.print_list()  # Should print 4->15->None
