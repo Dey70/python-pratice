45d66fe904cec87a9de3636287714efcca702c00
RAJDEEP DEY <rajdeep.x70@outlook.com>
2025-08-06 23:06:44 +0530

    learn LL: insert()

---
+++ DSA_BASICS/Linked-List/LL_10_insert().py
@@ -0,0 +1,106 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value, end = "->")
+            temp = temp.next
+        print("None")
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp   
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        
+        if self.length == 0:
+            self.tail = None
+        return temp
+    def get(self, index):
+        if index<0 or index>=self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+LL = LinkedList(4)
+print("Linked List: ")
+LL.append(15)
+LL.append(20)
+LL.print_list()
+
+LL.insert(1, 10)  # Insert 10 at index 1
+print("After inserting 10 at index 1:")
+LL.print_list()
+
+LL.insert(2, 20)  # Insert 20 at index 2
+print("After inserting 20 at index 2:")
+LL.print_list()
